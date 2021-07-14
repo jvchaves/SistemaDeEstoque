66d5a47a8079d475c3ce478e74a9d10e39d557df
João Victor Chaves <joao.victor.chaves159@gmail.com>
2021-07-14 13:08:12 -0300

    Criado validação para a data de movimentação

--- app/models/movimentacao.rb
+++ app/models/movimentacao.rb
@@ -4,6 +4,19 @@ class Movimentacao < ApplicationRecord
 
   validate :escolha_tipo, on: :create
   validate :verifica_estoque, on: :create
+  validate :valida_data, on: :create
+  def valida_data
+      min_valido = '01/01/2021'
+      max_valido = '31/01/2021'
+      min_valido = min_valido.to_datetime
+      max_valido = max_valido.to_datetime + 23.hours + 59.minute + 59.second
+    # binding.pry
+    if self.data_movimentacao >= min_valido and self.data_movimentacao <= max_valido
+      return true
+    else
+      errors.add(:base,"Data de movimentação Invalida")
+    end
+  end
   def escolha_tipo
     if    self.tipo == "S" and self.quantidade > 0
       self.quantidade	= (-1)*self.quantidade
